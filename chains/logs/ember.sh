6cf16dbb536cca147318bc2c1c128b05ec1b832a
Stefan Arseneau <arseneausm@gmail.com>
2025-09-25 15:56:31 -0400

    I have requested a frankly unreasonable compute nodes. Computers fear me, fish fear me

--- chains/logs/ember.sh
+++ chains/logs/ember.sh
@@ -4,7 +4,7 @@
 #$ -N ember
 #$ -o logs/$JOB_NAME.$JOB_ID.$TASK_ID.out
 #$ -e logs/$JOB_NAME.$JOB_ID.$TASK_ID.err
-#$ -t 1-256
+#$ -t 1-2048
 
 set -euo pipefail
 module load miniconda
@@ -12,7 +12,7 @@ module load miniconda
 
 echo "Host: $(hostname)"
 echo "Job:  $JOB_NAME  ID: $JOB_ID  Task: $SGE_TASK_ID"
-echo "Cmd:  ember fit-sed /project/mesaelm/omnidwarf/GaiaDR3_white_dwarfs.pqt /project/mesaelm/omnidwarf/chains/ --xpphoto --synthetic --mcmc --source-id GaiaEDR3 --ra RA_ICRS --dec DE_ICRS --parallax PlxZPCorr --parallax-error e_Plx --meanav meanAV --numtasks 256"
+echo "Cmd:  ember fit-sed /project/mesaelm/omnidwarf/GaiaDR3_white_dwarfs.pqt /project/mesaelm/omnidwarf/chains/ --xpphoto --synthetic --mcmc --source-id GaiaEDR3 --ra RA_ICRS --dec DE_ICRS --parallax PlxZPCorr --parallax-error e_Plx --meanav meanAV --numtasks 2048"
 
 # Run the command
-ember fit-sed /project/mesaelm/omnidwarf/GaiaDR3_white_dwarfs.pqt /project/mesaelm/omnidwarf/chains/ --xpphoto --synthetic --mcmc --source-id GaiaEDR3 --ra RA_ICRS --dec DE_ICRS --parallax PlxZPCorr --parallax-error e_Plx --meanav meanAV --numtasks 256
+ember fit-sed /project/mesaelm/omnidwarf/GaiaDR3_white_dwarfs.pqt /project/mesaelm/omnidwarf/chains/ --xpphoto --synthetic --mcmc --source-id GaiaEDR3 --ra RA_ICRS --dec DE_ICRS --parallax PlxZPCorr --parallax-error e_Plx --meanav meanAV --numtasks 2048
--- run_chains.py
+++ run_chains.py
@@ -4,7 +4,7 @@ import os
 
 def downloadNGF():
     tap_service = pyvo.dal.TAPService("http://TAPVizieR.u-strasbg.fr/TAPVizieR/tap/")
-    QUERY = """select GaiaEDR3, RA_ICRS, DE_ICRS, Plx - ZPCorr as PlxZPCorr, e_Plx, meanAV 
+    QUERY = """select GaiaEDR3, RA_ICRS, DE_ICRS, Plx - ZPCor as PlxZPCorr, e_Plx, meanAV 
             from \"J/MNRAS/508/3877/maincat\" """
     data = tap_service.search(QUERY).table.to_pandas()
     data.to_parquet("GaiaDR3_white_dwarfs.pqt")
@@ -17,4 +17,4 @@ if __name__ == "__main__":
         print("File 'GaiaDR3_white_dwarfs.pqt' not found, downloading!")
         data = downloadNGF()
 
-    os.system("ember submit fit-sed /project/mesaelm/omnidwarf/GaiaDR3_white_dwarfs.pqt /project/mesaelm/omnidwarf/chains/ --xpphoto --synthetic --mcmc --source-id GaiaEDR3 --ra RA_ICRS --dec DE_ICRS --parallax PlxZPCorr --parallax-error e_Plx --meanav meanAV --numtasks 256")
+    os.system("ember submit fit-sed /project/mesaelm/omnidwarf/GaiaDR3_white_dwarfs.pqt /project/mesaelm/omnidwarf/chains/ --xpphoto --synthetic --mcmc --source-id GaiaEDR3 --ra RA_ICRS --dec DE_ICRS --parallax PlxZPCorr --parallax-error e_Plx --meanav meanAV --numtasks 512")
